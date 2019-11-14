30b4ca3ed434aa65b00b726d981dd644f122d250
Page <hyq5436@hotmail.com>
2019-11-14 20:39:41 +0800

    qt playgroud

---
+++ qt_playground/GifLoadingDemo/ui_widget.h
@@ -0,0 +1,57 @@
+/********************************************************************************
+** Form generated from reading UI file 'widget.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.3
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WIDGET_H
+#define UI_WIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Widget
+{
+public:
+    QPushButton *load;
+    QPushButton *stop;
+
+    void setupUi(QWidget *Widget)
+    {
+        if (Widget->objectName().isEmpty())
+            Widget->setObjectName(QString::fromUtf8("Widget"));
+        Widget->resize(400, 300);
+        load = new QPushButton(Widget);
+        load->setObjectName(QString::fromUtf8("load"));
+        load->setGeometry(QRect(90, 110, 93, 28));
+        stop = new QPushButton(Widget);
+        stop->setObjectName(QString::fromUtf8("stop"));
+        stop->setGeometry(QRect(230, 110, 93, 28));
+
+        retranslateUi(Widget);
+
+        QMetaObject::connectSlotsByName(Widget);
+    } // setupUi
+
+    void retranslateUi(QWidget *Widget)
+    {
+        Widget->setWindowTitle(QApplication::translate("Widget", "Widget", nullptr));
+        load->setText(QApplication::translate("Widget", "Load", nullptr));
+        stop->setText(QApplication::translate("Widget", "Stop", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Widget: public Ui_Widget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WIDGET_H

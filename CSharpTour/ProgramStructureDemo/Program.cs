2e68b8ec5b904f00daa450ab49de9a1991a2c0fb
Page <hyq5436@hotmail.com>
2019-11-17 23:01:57 +0800

    CSharp Tour

---
+++ CSharpTour/ProgramStructureDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Acme.Collections
+{
+    public class Stack
+    {
+        Entry top;
+        public void Push(object data)
+        {
+            top = new Entry(top, data);
+        }
+
+        public object Pop()
+        {
+            if (top == null)
+            {
+                throw new InvalidOperationException();
+            }
+            object result = top.data;
+            top = top.next;
+            return result;
+        }
+
+        class Entry
+        {
+            public Entry next;
+            public object data;
+            public Entry(Entry next, object data)
+            {
+                this.next = next;
+                this.data = data;
+            }
+        };
+    }
+}
+namespace ProgramStructureDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CSharpTour/ReferenceDll/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Acme.Collections;
+
+namespace ReferenceDll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+            s.Push(1);
+            s.Push(10);
+            s.Push("HelloWorld");
+            Console.WriteLine(s.Pop());
+            Console.WriteLine(s.Pop());
+            Console.WriteLine(s.Pop());
+        }
+    }
+}

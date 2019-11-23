e01786d56e6c0dd029970768ce57fa3d4ddca721
Page <hyq5436@hotmail.com>
2019-11-23 22:20:59 +0800

    C# Tour demo

---
+++ CSharpTour/UsingDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace UsingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (TextWriter w = File.CreateText("test.txt"))
+            {
+                w.WriteLine("Line one");
+                w.WriteLine("Line two");
+                w.WriteLine("Line three");
+            }
+        }
+    }
+}
---
+++ CSharpTour/YieldDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace YieldDemo
+{
+    class Program
+    {
+        static System.Collections.Generic.IEnumerable<int> Range(int start, int end)
+        {
+            for(int i = start; i < end; i++)
+            {
+                yield return i;
+            }
+        }
+        static void Main(string[] args)
+        {
+            foreach(int i in Range(-10, 10))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}

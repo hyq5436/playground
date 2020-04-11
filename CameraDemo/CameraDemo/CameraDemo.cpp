282fbbd99c2951a0583f10ae891d86af578c4f75
Page <hyq5436@hotmail.com>
2020-04-11 23:51:35 +0800

    摄像头预览demo

---
+++ CameraDemo/CameraDemo/CameraDemo.cpp
@@ -0,0 +1,44 @@
+﻿#include "CameraDemo.h"
+#include <QDebug>
+#include <QPushButton>
+#include <QCamera>
+#include <QCameraInfo>
+#include <QCameraViewfinder>
+
+bool checkCameraAvailability()
+{
+    const QList<QCameraInfo> cameras = QCameraInfo::availableCameras();
+    for (const QCameraInfo &cameraInfo : cameras)
+        qDebug() << cameraInfo.deviceName();
+
+    if (QCameraInfo::availableCameras().count() > 0)
+        return true;
+    else
+        return false;
+}
+
+CameraDemo::CameraDemo(QWidget *parent)
+    : QWidget(parent)
+{
+    ui.setupUi(this);
+    const QList<QCameraInfo> cameras = QCameraInfo::availableCameras();
+    for (const QCameraInfo &cameraInfo : cameras)
+        qDebug() << cameraInfo.deviceName();
+
+    m_pCamera = new QCamera(this);
+    connect(ui.btnStop, &QPushButton::pressed, this, [this]() {
+        m_pCamera->stop();
+    });
+    connect(ui.btnShowCamera, &QPushButton::pressed, this, [this]() {
+        if (!checkCameraAvailability())
+        {
+            qDebug() << "Camera Not Avaliable";
+        }
+        m_pCamera->setCaptureMode(QCamera::CaptureVideo);
+        m_pCamera->setViewfinder(ui.widget);
+        qDebug() << "errorCode: " << m_pCamera->error();
+        qDebug() << "error: " << m_pCamera->errorString();
+        qDebug() << "status: " << m_pCamera->status();
+        m_pCamera->start();
+    });
+}
---
+++ CameraDemo/CameraDemo/CameraDemo.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include <QtWidgets/QWidget>
+#include "ui_CameraDemo.h"
+
+class QCamera;
+class CameraDemo : public QWidget
+{
+    Q_OBJECT
+
+public:
+    CameraDemo(QWidget *parent = Q_NULLPTR);
+
+private:
+    Ui::CameraDemoClass ui;
+    QCamera* m_pCamera;
+};

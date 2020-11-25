f06ba8a57859af39e5781ff337e14e204d1d99c0
Page <hyq5436@hotmail.com>
2020-11-25 16:38:49 +0800

    add cpack demo

---
+++ cpack_demo/README.txt
@@ -0,0 +1,10 @@
+$ mkdir build
+$ cd build
+$ cmake ..
+$ make
+$ cpack
+
+deb package will generated in build dir
+
+
+https://blog.usejournal.com/creating-debian-packages-cmake-e519a0186e87
---
+++ cpack_demo/src/main.cpp
@@ -0,0 +1,11 @@
+#include"add_num.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+   A2DD a(1, 2);
+   int res = a.getSum();
+   cout << res << endl;
+   return 0;
+}
